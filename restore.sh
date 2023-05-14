90f741cd6eb12f6798e90c0b0987c53577c62605
marcuscastelo <marcuscastelo@usp.br>
2023-05-14 20:37:15 -0300

    clear_cache bin

--- restore.sh
+++ restore.sh
@@ -4,11 +4,13 @@ BACKUP_FOLDER='backup/'
 [ -d "$BACKUP_FOLDER" ] || (echo "Backup folder not found: '$BACKUP_FOLDER'" && exit 1)
 [ -t $? ] || exit 1
 
-# Initially, I am going to test with a low-impact folder
-FOLDER='usr/local/bin/'
+function restore_folder() {
+  FOLDER="$1"
+  cmd="sudo rsync -rvcauEgoptUWX $(pwd)/$BACKUP_FOLDER$FOLDER /$FOLDER"
+  echo "> $cmd"
+  $cmd
+  echo
+}
 
-echo "sudo rsync -rvcauEgoptUWX $(pwd)/$BACKUP_FOLDER$FOLDER /$FOLDER"
-
-FOLDER='usr/share/X11/xkb/symbols/'
-
-echo "sudo rsync -rvcauEgoptUWX $(pwd)/$BACKUP_FOLDER$FOLDER /$FOLDER"
+restore_folder 'usr/local/bin/'
+restore_folder 'usr/share/X11/xkb/symbols/'
